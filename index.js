c01c0b01352f2a3d5f96eec244f04f6458bf44da
Slobodan <slobodan@cloudhorizon.com>
2015-05-12 01:21:15 +0200

    Add support for non-node-style functions and add more tests.

--- index.js
+++ index.js
@@ -11,31 +11,40 @@ const defer = function() {
 }
 
 // Make callback function
-const prepareCallback = function(deferred) {
-  return function (err) {
-    if (err) {
-      deferred.reject(err)
+const prepareCallback = function(deferred, withoutErrorAttr) {
+
+  // By default start with a second attribute
+  let base = 2
+
+  // But if it's not node style callback switch to the first one
+  if (withoutErrorAttr)
+    --base
+
+  return function () {
+    // If node style function return an error
+    if (arguments[0] && !withoutErrorAttr) {
+      deferred.reject(arguments[0])
     }
 
-    else if (arguments.length < 2) {
+    else if (arguments.length < base) {
       deferred.resolve()
     }
 
-    else if (arguments.length === 2) {
-      deferred.resolve(arguments[1])
+    else if (arguments.length === base) {
+      deferred.resolve(arguments[--base])
     }
 
     else {
-      deferred.resolve(Array.prototype.slice.call(arguments, 1))
+      deferred.resolve(Array.prototype.slice.call(arguments, --base))
     }
   }
 }
 
 // Transform function with callback to generator
-const generatorify = module.exports = function(fn, context) {
+const generatorify = module.exports = function(fn, context, withoutErrorAttr) {
   return function() {
     const deferred = defer(),
-          callback = prepareCallback(deferred),
+          callback = prepareCallback(deferred, withoutErrorAttr),
           args     = Array.prototype.slice.call(arguments).concat(callback)
 
     fn.apply(context, args)
---
+++ test/node-style.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const test         = require('tap').test
+const generatorify = require('../index')
+
+function funcWithCallbacks(arg, callback) {
+  if (!callback)
+    return false
+
+  if (arg === 0) {
+    callback(null)
+  } else if (arg === 1) {
+    callback(null, arg + 1);
+  } else {
+    callback(null, arg + 1, arg + 2)
+  }
+}
+
+function *generatorFunc(arg) {
+  let genFunction = generatorify(funcWithCallbacks)
+
+  let result = yield genFunction(arg)
+
+  return result
+}
+
+test('Callback with 0, 1 and more argument, node style function (`function(err, arg1, arg2, ...) { ... }`)', function(t) {
+  t.plan(3)
+
+  let genFunc1     = generatorFunc(0)
+  let genFunc2     = generatorFunc(1)
+  let genFunc3     = generatorFunc(2)
+  let genFuncNext1 = genFunc1.next()
+  let genFuncNext2 = genFunc2.next()
+  let genFuncNext3 = genFunc3.next()
+
+  genFuncNext1.value.then(function(response) {
+    t.equal(response, undefined, 'Request should be undefined')
+  })
+
+  genFuncNext2.value.then(function(response) {
+    t.equal(response, 2, 'Response should be 2')
+  })
+
+  genFuncNext3.value.then(function(response) {
+    t.similar(response, [3, 4], 'Request should be [3, 4]')
+  })
+})
